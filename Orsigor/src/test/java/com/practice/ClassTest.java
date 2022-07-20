6f20d7f915f6ce57560a4a7a0a716675ebf73b94
Win10 <Win10@DESKTOP-8A44VM7>
2022-07-20 17:08:00 +0530

    commit1

---
+++ Orsigor/src/test/java/com/practice/ClassTest.java
@@ -0,0 +1,15 @@
+package com.practice;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class ClassTest {
+	
+	@Test
+	public void clssTest() {
+		Reporter.log("test1", true);
+		Reporter.log("test2", true);
+		Reporter.log("test3", true);
+		Reporter.log("test4", true);
+	}
+
+}
